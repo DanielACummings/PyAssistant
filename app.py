f0a737df03ccd555860f91a6313835f16f933dc6
Daniel <danielac@protonmail.com>
2023-05-29 16:59:25 -0600

    Save progress

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# SQL configs
+app.config['SQLALCHEMY_DATABASE_URI'] = 'DanielACummings.mysql.pythonanywhere-services.com'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
 
 # Index/PyAssistant
 @app.get('/')
@@ -8,9 +15,21 @@ def index():
     return render_template('index.html')
 
 
-
 # QuickList
-ql_lists = ['test1', 'test2']
+class Ql_list(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+
+    def __init__(self, content):
+        self.content = content
+
+    def __repr__(self):
+        return f'<Content {self.content}>'
+
+db.create_all()
+
+ql_lists = Ql_list.query.all()
+
 
 @app.get('/quick-list')
 def ql_home():
@@ -19,10 +38,14 @@ def ql_home():
 
 @app.post('/add-list')
 def ql_add_list():
-    ql_list = request.form['ql_list']
-    ql_lists.append(ql_list)
-    return redirect(url_for('ql_home'))
+    form_data = request.form['ql_list']
+    if not form_data:
+        return 'Error'
 
+    ql_list = Ql_list(form_data)
+    db.session.add(ql_list)
+    db.session.commit()
+    return redirect(url_for('ql_home'))
 
 
 if __name__ == '__main__':
