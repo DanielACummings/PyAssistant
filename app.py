269586070300045df2d7234bc1d90261d83ba42a
Daniel <danielac@protonmail.com>
2023-05-29 15:50:04 -0600

    Lists can be added

--- app.py
+++ app.py
@@ -7,9 +7,23 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+
 # QuickList
 ql_lists = ['test1', 'test2']
 
 @app.get('/quick-list')
-def ql_base():
+def ql_home():
     return render_template('quick-list.html', ql_lists=ql_lists)
+
+
+@app.post('/add-list')
+def ql_add_list():
+    ql_list = request.form['ql_list']
+    ql_lists.append(ql_list)
+    return redirect(url_for('ql_home'))
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
